6902899e6dc98e8816b36351330d3eda494fd674
Ephraim-Kamau <kephraim19@gmail.com>
2019-04-27 17:56:03 +0300

    Add navbar

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from .config import DevConfig
+from flask_bootstrap import Bootstrap
+
+
+# Initializing application
+app = Flask(__name__)
+
+# Setting up configuration
+app.config.from_object(DevConfig)
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
+
+
+from app import views
