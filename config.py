8978c9dd26cf4b787d0f42c32a9e49b50c2cf15e
Ephraim-Kamau <kephraim19@gmail.com>
2019-04-29 15:11:21 +0300

    Add the initial migration

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
     '''
     General configuration parent class
     '''
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ephraim:server123@localhost/blogs'
 
