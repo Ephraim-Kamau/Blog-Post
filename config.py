2aff2f79a27f49e1941c476997cd443b9507f34e
Ephraim-Kamau <kephraim19@gmail.com>
2019-04-29 22:01:52 +0300

    Add conversion of Markdown to HTML

--- config.py
+++ config.py
@@ -16,6 +16,10 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ephraim:server123@localhost/blogs_test'
 
